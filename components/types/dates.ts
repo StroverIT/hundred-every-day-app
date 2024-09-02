92ecc24a5e65bf4e87e8ed5ef38adc545f85d256
StroverIT <StroverIT>
2024-09-02 14:52:18 +0300

    default formatter and date of the weeks type enum

---
+++ components/types/dates.ts
@@ -0,0 +1,11 @@
+export enum DateWeekType  {
+    Monday = "Monday",
+    Tuesday = "Tuesday",
+    Wednesday = "Wednesday",
+    Thursday = "Thursday",
+    Friday = "Friday",
+    Saturday = "Saturday",
+    Sunday = "Sunday"
+}
+
+export const defaultFormatDate = "YYYY-MM-DD"
