fb65cb7fcf7abf4a26d090a84436578b3e45d594
StroverIT <StroverIT>
2024-09-02 14:52:48 +0300

    Fixing the problem instead of example is authentication

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -3,6 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import authenticationSlice from '@/lib/store/features/authentication/authenticationSlice';
 export const store = configureStore({
   reducer: {
-    example: authenticationSlice,
+    authentication: authenticationSlice,
   },
 });
