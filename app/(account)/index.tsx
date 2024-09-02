925277751cbeae667a682878dac94a7815c86cdb
StroverIT <StroverIT>
2024-09-02 14:50:27 +0300

    Updating and getting training

--- app/(account)/index.tsx
+++ app/(account)/index.tsx
@@ -1,12 +1,20 @@
+import { getTraining, updateTraining } from "@/API/training";
 import Training from "@/components/pages/account/Training/Training";
-import { trainingTypes } from "@/components/pages/account/Training/types";
+import {
+  trainingTypes,
+  trainingTypesUpdate,
+  updateValueHandlerType,
+} from "@/components/pages/account/Training/types";
+import { defaultFormatDate } from "@/components/types/dates";
 import moment from "moment";
-import React, { useState } from "react";
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import React, { useEffect, useState } from "react";
+import { Button, SafeAreaView, StyleSheet } from "react-native";
+import { Calendar } from "react-native-calendars";
+import { useSelector } from "react-redux";
 
 export default function index() {
-  const [selected, setSelected] = useState(moment().format("YYYY-MM-DD"));
+  const { user } = useSelector((state: any) => state.authentication) || {};
+  const [selected, setSelected] = useState(moment().format(defaultFormatDate));
   const [isCalendarOpen, setCalendarOpen] = useState(false);
   const [trainingData, setTrainingData] = useState({
     [trainingTypes.pushUps]: {
@@ -22,6 +30,35 @@ export default function index() {
       maxReps: 0,
     },
   });
+
+  const updateValueHandler: updateValueHandlerType = (type, increment) => {
+    const newValue = parseInt(increment) + trainingData[type].currentReps;
+
+    updateTraining({
+      userId: user.id,
+      selected,
+      type: trainingTypesUpdate[type],
+      increment: newValue,
+    });
+    
+    setTrainingData((prev) => ({
+      ...prev,
+      [type]: {
+        ...prev[type],
+        currentReps: newValue,
+      },
+    }));
+  };
+
+  useEffect(() => {
+    getTraining({
+      userId: user.id,
+
+      selected,
+      setTrainingData,
+    });
+  }, [selected]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Button
@@ -46,16 +83,19 @@ export default function index() {
         typeOfTraining={trainingTypes.pushUps}
         currentReps={trainingData[trainingTypes.pushUps].currentReps}
         maxReps={trainingData[trainingTypes.pushUps].maxReps}
+        updateValueHandler={updateValueHandler}
       />
       <Training
         typeOfTraining={trainingTypes.sitUps}
         currentReps={trainingData[trainingTypes.sitUps].currentReps}
         maxReps={trainingData[trainingTypes.sitUps].maxReps}
+        updateValueHandler={updateValueHandler}
       />
-        <Training
+      <Training
         typeOfTraining={trainingTypes.crunches}
         currentReps={trainingData[trainingTypes.crunches].currentReps}
         maxReps={trainingData[trainingTypes.crunches].maxReps}
+        updateValueHandler={updateValueHandler}
       />
     </SafeAreaView>
   );
