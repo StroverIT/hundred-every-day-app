bf3ab9bdd801d57c958c3f53616b62fa666d29b1
StroverIT <StroverIT>
2024-09-02 14:52:05 +0300

    in TrainingProps adding updateValueHandler; trainingTypesUpdate to set updating values data for supabase

--- components/pages/account/Training/types.ts
+++ components/pages/account/Training/types.ts
@@ -1,13 +1,22 @@
+export type updateValueHandlerType = (type: trainingTypes, value: string) => void;
+
 export type TrainingProps = {
   maxReps: number;
   currentReps: number;
   typeOfTraining: trainingTypes;
+  updateValueHandler: updateValueHandlerType;
 };
 
 export enum trainingTypes {
   pushUps = "Push Ups",
   sitUps = "Sit Ups",
   crunches = "Crunches",
+}
+
+export const trainingTypesUpdate = {
+  [trainingTypes.pushUps]: "pushUpsReps",
+  [trainingTypes.sitUps]: "sitUpsReps",
+  [trainingTypes.crunches]: "crunchesReps",
 }
 
 export enum typeOfValueCounter  {
