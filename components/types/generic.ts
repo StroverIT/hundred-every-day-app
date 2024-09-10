0e6c219daba70e7d3e1697e4b93c55017b8222d8
StroverIT <StroverIT>
2024-09-10 18:41:27 +0300

    Adding redux types

---
+++ components/types/generic.ts
@@ -0,0 +1,12 @@
+
+
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import { store } from '@/lib/store';
+
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
